d73e00e02e3277212bdb5752f99c0387c49d7c3f
Shruti Agrawal <shruti98932@gmail.com>
2021-06-23 11:42:06 -0400

    Extra credit completed

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -23,26 +23,33 @@
 //Thrown by a single player
 //Over 3 rounds og game of RPS.
 
-var rockPaperScissors = function () {
+var rockPaperScissors = function (rounds) {
   // TODO: your solution here
   //3 given inputs
   var input = ['R', 'P', 'S'];
   // Possible combination of outputs
   var output = [];
 
-  //Generate each combination
-  for (var i = 0; i <input.length; i++) {
-    for( var j=0; j <input.length; j++) {
-      for (var k=0; k <input.length; k++) {
-        output.push(input[i] + input[j] + input[k]);
+  var innerFunction = function(currentString, roundsLeft) {
+    // Base case when we reach leaves or rounds exhausts
+    if(roundsLeft === 0) {
+      output.push(currentString);
+    } else {
+      //iterate for each root
+      for( var i=0; i< input.length; i++) {
+        //concat each root with other combinations
+        var newString = currentString.concat(input[i]);
+        innerFunction(newString, roundsLeft-1);
       }
     }
   }
-
+  innerFunction("", rounds);
   return output;
 };
 
 //invoking the main function
-var result = rockPaperScissors();
+var result = rockPaperScissors(3);
 console.log(result);
+var result1 = rockPaperScissors(2);
+console.log(result1);
 
