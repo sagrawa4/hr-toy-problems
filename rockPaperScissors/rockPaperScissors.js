2365b3795c2432bffd7321ab1c70bee214041215
Shruti Agrawal <shruti98932@gmail.com>
2021-06-22 22:56:50 -0400

    Toy1 Completed

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,31 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+//Generate a function.
+//Function should generate a seq of throws.
+//Thrown by a single player
+//Over 3 rounds og game of RPS.
+
+var rockPaperScissors = function () {
   // TODO: your solution here
+  //3 given inputs
+  var input = ['R', 'P', 'S'];
+  // Possible combination of outputs
+  var output = [];
+
+  //Generate each combination
+  for (var i = 0; i <input.length; i++) {
+    for( var j=0; j <input.length; j++) {
+      for (var k=0; k <input.length; k++) {
+        output.push(input[i] + input[j] + input[k]);
+      }
+    }
+  }
+
+  return output;
 };
+
+//invoking the main function
+var result = rockPaperScissors();
+console.log(result);
 
