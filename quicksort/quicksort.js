860415149a40f71c06501112dca125da050bff5e
Shruti Agrawal <shruti98932@gmail.com>
2021-06-29 23:03:37 -0400

    Implemeted Quick sort

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -11,5 +11,47 @@
  */
 
 
-var quicksort = function(array) {
-};
+ var quicksort = function(array) {
+  if(array.length === 0) return [];
+
+  var outterFunction = function(array, start, end) {
+      var pivot = Array[Math.floor((start+end)/2)];
+        while(start <= end) {
+            while(array[start] < pivot) {
+                start++;
+            };
+            while(array[end] > pivot) {
+                end++;
+            };
+            if(start <= end) {
+                var temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        return start;
+  }
+
+     var innerFunction = function(array, start, end) {
+     var index;
+     if(array.length > 1){
+        index = outterFunction(array, start, end)
+        }
+     if(start < index - 1) {
+         innerFunction(array, start, index - 1);
+     };
+     if(index < end) {
+         innerFunction(array, index, end)
+     }
+     console.log(array)
+     return array;
+     }
+
+     var result = innerFunction(array, 0, array.length-1);
+     return result;
+ };
+
+ var res = quicksort([4,3,2]);
+ console.log(res);
